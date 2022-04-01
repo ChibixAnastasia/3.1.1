c3596cf11055c263ed1416341dae3ac71a19b0c3
AnastasiyaChibis <chibisovanastena@gmail.com>
2022-04-01 18:32:19 +0300

    1

--- src/main/java/com/JAVAmentor311/Application.java
+++ src/main/java/com/JAVAmentor311/Application.java
@@ -3,8 +3,9 @@ package com.JAVAmentor311;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan ("com.JAVAmentor311.repository")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
--- src/main/java/com/JAVAmentor311/repository/UserRepository.java
+++ src/main/java/com/JAVAmentor311/repository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Transactional
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query(value = "update users set name = :name , age = :age where id = :id",nativeQuery = true)
