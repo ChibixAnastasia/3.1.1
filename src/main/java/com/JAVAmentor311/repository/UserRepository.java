98858ffd9e48ccf6ee2f0e648846a44a6b03d462
AnastasiyaChibis <chibisovanastena@gmail.com>
2022-04-02 18:13:20 +0300

    1

--- src/main/java/com/JAVAmentor311/repository/UserRepository.java
+++ src/main/java/com/JAVAmentor311/repository/UserRepository.java
@@ -4,13 +4,14 @@ import com.JAVAmentor311.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Transactional
-@Repository
+@EnableJpaRepositories("com.JAVAmentor311.repository")
 public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query(value = "update users set name = :name , age = :age where id = :id",nativeQuery = true)
